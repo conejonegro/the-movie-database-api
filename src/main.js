eb3ec8163e4664990b23224b0da927d82a55ce20
Luis Rosales <conejoconsal@hotmail.com>
2022-08-03 22:20:30 -0500

    estructura de datos terminada 2

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log("Hola Mundo");
+const API_KEY = 'api_key=d35b24b361166e540ee6c082ddecd6bf';
+const URL = 'https://api.themoviedb.org/3/';
+
+const test_movie_endpoint = 'https://api.themoviedb.org/3/movie/550?api_key=d35b24b361166e540ee6c082ddecd6bf';
+
+async function apiCall(){
+    const response = await fetch(test_movie_endpoint);
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+
+apiCall()
